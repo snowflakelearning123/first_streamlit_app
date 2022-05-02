53a76b9abcf3a673b9fc53b825470e5d0f07e249
snowflakelearning123 <104784585+snowflakelearning123@users.noreply.github.com>
2022-05-02 16:56:03 -0500

    Update streamlit_app.py
    added Avocado and Strawberries to pre-populate multi-select list

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,7 @@ my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/da
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 #multi-select list
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado','Strawberries'])
 
 #display the table
 streamlit.dataframe(my_fruit_list)
