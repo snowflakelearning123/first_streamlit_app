da9012b5cfc5be502d882cf8266988024037eef8
snowflakelearning123 <104784585+snowflakelearning123@users.noreply.github.com>
2022-05-02 17:12:27 -0500

    Update streamlit_app.py
    changed the display table to show selected fruits

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,8 @@ my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/da
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 #multi-select list
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado','Strawberries'])
+fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado','Strawberries'])
+fruits_to_show = my_fruit_list.loc[fruits_selected]
 
-#display the table
-streamlit.dataframe(my_fruit_list)
+#display the table of selected fruits
+streamlit.dataframe(fruits_to_show)
