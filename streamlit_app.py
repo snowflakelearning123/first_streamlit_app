97760d528bcb9720b7eb077923c60b5c7ae2087f
snowflakelearning123 <104784585+snowflakelearning123@users.noreply.github.com>
2022-05-04 15:27:32 -0500

    Update streamlit_app.py
    fixed typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,5 +39,5 @@ my_cur = my_cnx.cursor()
 my_cur.execute("select current_user(), current_account(), current_region()")
 my_data_row = my_cur.fetchone()
 streamlit.text("Hello from snowflake:")
-streamlit.txt(my_data_row)
+streamlit.text(my_data_row)
 
